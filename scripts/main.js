db4b8be159b66e4ed5f009dac01e601959eac4bb
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2020-01-15 12:22:12 +0100

    Just a push

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,10 @@ document.getElementById('run').addEventListener("click", function () {
 
     let userInput = document.getElementById('input').value;
 
-
     fetch(`https://pokeapi.co/api/v2/pokemon/` + userInput)
         .then(res => res.json())
         .then(pokemon => {
             console.log(pokemon);
-            console.log(userInput);
         })
         .catch(function (err) {
             console.log(err, err.response);
