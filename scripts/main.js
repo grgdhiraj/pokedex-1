81b6ca073126c0e0738e81714fc38325e192541c
Irina-sku <irina.a.skuratova@gmail.com>
2020-01-15 12:33:34 +0100

    pls work

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,7 @@ document.getElementById('run').addEventListener("click", function () {
 
     let userInput = document.getElementById('input').value;
 
-<<<<<<< HEAD
-    fetch(`https://pokeapi.co/api/v2/pokemon/` + userInput)
-=======
-
     fetch(`https://pokeapi.co/api/v2/pokemon/` + userInput.toLowerCase())
->>>>>>> f097394c8b5f37231d6e2ffef73e6777b9361577
         .then(res => res.json())
         .then(pokemon => {
             console.log(pokemon);
